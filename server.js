09b6f28b44a50239b6f4be6cb09d7a2500ad3184
Jerry Lee <jerrylee1157@gmail.com>
2020-03-16 21:30:00 -0700

    changed file structure

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const cooldowns = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
+    const command = require(`./src/commands/${file}`);
 
     // set a new item in the Collection
     // with the key as the command name and the value as the exported module
@@ -40,11 +40,13 @@ client.on("message", message => {
     const timestamps = cooldowns.get(command.name);
     const cooldownAmount = (command.cooldown || 3) * 1000;
     if (timestamps.has(message.author.id)) {
-        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+        const expirationTime = timestamps.get(message.author.id)
+            + cooldownAmount;
 
         if (now < expirationTime) {
             const timeLeft = (expirationTime - now) / 1000;
-            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+            return message.reply(`please wait ${timeLeft.toFixed(1)}
+             more second(s) before reusing the \`${command.name}\` command.`);
         }
     }
     timestamps.set(message.author.id, now);
