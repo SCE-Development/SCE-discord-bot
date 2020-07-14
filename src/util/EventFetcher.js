11ab3beb7b170763104c8f62660f984a548d70bb
Mark Brogan <47675634+markbrogan13@users.noreply.github.com>
2020-07-13 21:56:57 -0700

    Add Embed Functionality (#43) (#46)

--- src/util/EventFetcher.js
+++ src/util/EventFetcher.js
@@ -1,10 +1,47 @@
 const fetch = require('node-fetch');
 const { SCE_GOOGLE_API_URL } = require('../../config.json');
+const { EVENTS_CAL } = require('../../config.json');
 
-const info = ':information_source:';
 const calendar = ':calendar_spiral:';
-const pushPin = ':round_pushpin';
+const pushPin = ':round_pushpin:';
 const clock = ':clock3:';
+
+/**
+ * Parses date to a human readable form
+ * @param {Date} dateTime 
+ */
+function parseDate(dateTime) {
+  const todaysDate = new Date();
+  const days = 
+    ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+  let months = 
+    ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June',
+      'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+  const eventDay = days[dateTime.getDay()];
+  const eventMonth = months[dateTime.getMonth()];
+  if (Math.abs(todaysDate.getDate() - dateTime.getDate()) > 6) {
+    return `${clock} ${eventDay} 
+            @ ${dateTime.getHours()}:${dateTime.getMinutes}`;
+  } else if (todaysDate.getMonth() == dateTime.getMonth()) {
+    return `${clock} ${eventDay} the ${dateTime.getDate()} 
+            @ ${dateTime.getHours()}:${dateTime.getMinutes}`;
+  } else {
+    return `${clock} ${eventDay} ${eventMonth}, ${dateTime.getDate()} 
+            @ ${dateTime.getHours()}:${dateTime.getMinutes}`;
+  }
+}
+
+/**
+ * Formats the event data into human readable content
+ * @param {Object} event 
+ */
+function formatEventInfo(event) {
+  if (!(typeof event.location === 'undefined')) {
+    return `${pushPin} ${event.location}\n${parseDate(event.start.dateTime)}`;
+  } else {
+    return `${parseDate(event.start.dateTime)}`;
+  }
+}
 
 /**
  * fetches event from SCE API and populates an array of events
@@ -16,16 +53,13 @@ function fetchEvents(numOfEvents) {
     let eventData = [];
     let fields = [];
     // eslint-disable-next-line
-      fetch(`${SCE_GOOGLE_API_URL}/api/mailer/getCalendarEvents?calendarID=primary&numOfEvents=${numOfEvents}`)
+      fetch(`${SCE_GOOGLE_API_URL}/api/Calendar/getCalendarEvents?calendarID=${EVENTS_CAL}&numOfEvents=${numOfEvents}`)
       .then(data => data.json())
       .then(data => {
         eventData = data.calendarEvents;
         fields = eventData.map(event => ({
           'name': `${calendar} ${event.summary}`,
-          'value': 
-                  `${info} \`${event.description || 'No Info'}\` 
-                    \n${pushPin} \`${event.location || 'No Location'}\` 
-                    \n${clock} \`${event.start.dateTime}\``,
+          'value': `${formatEventInfo(event)}`,
           'inline': true,
         }));
         resolve(fields);
