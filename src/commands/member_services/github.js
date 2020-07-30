fc1fcc55eb050804cdbe98d9cb45bee69e59ca56
unknown <ccastro715@gmail.com>
2020-07-29 23:48:08 -0700

    Add Github commands and embed functionality
    Added github command w/ leaderboard functionality
    
    Fixed indexing format
    
    Implemented embed for leaderboard
    
    Created function to retrieve pull requests
    
    Updated data resolved in promise
    
    Improved embed for leaderboard
    
    Implemented command for Pull Requests
    
    Added link to pull request url
    
    Updated FetchPullRequest to return embed field
    
    Added function to fetch commits from Core-V4
    
    Implemented commands for leaderboard, pr, commits
    
    Fixed issues with eslint
    
    Added comments to GithubFetcher
    
    Added Github url to config.example.json
    
    UI improvements for leaderboard
    
    Added helper class GithubMessageGenerator
    
    Refactored code to utilize helper class
    
    Moved GithubMessageGenerator

---
+++ src/commands/member_services/github.js
@@ -0,0 +1,77 @@
+const Discord = require('discord.js');
+const Command = require('../Command');
+const { GithubMessageGenerator } = require('../../util/GithubMessageGenerator');
+
+/**
+ * commands that grabs Github data from SCE server
+ * pr <repoName> -> Open Pull Requests
+ * leaderboard <repoName> -> Top 5 contributors
+ * commits <repoName> <numOfCommits> -> Merged Commits
+ */
+module.exports = new Command({
+  name: 'github',
+  description: 'Displays following information from SCE github repos:\
+  Contributor leaderboard, Pull requests, merged commits',
+  category: 'github',
+  aliases: ['git'],
+  permissions: 'general',
+
+  execute: (message, args) => {
+    const messageGenerator = new GithubMessageGenerator();
+
+    switch(args[0]) {
+    case 'pr':
+      messageGenerator.generatePullRequestMessage(args[1])
+        .then(prMessage => {
+          message.channel.send(prMessage);
+        })
+        .catch(_ => {
+          message.channel.send(_);
+        });
+      break;
+  
+    case 'leaderboard':
+      messageGenerator.generateLeaderboardMessage(args[1])
+        .then(leaderboardMessage => {
+          leaderboardMessage.forEach(embed => {
+            message.channel.send(embed);
+          });
+        })
+        .catch(_ => {
+          message.channel.send(_);
+        });
+      break;
+
+    case 'commits':
+      // Check if 3rd argument (optional) is a number
+      if(args[2] && isNaN(args[2])) {
+        message.channel.send(
+          `Invalid Value: ${args[2]} Please enter a number`
+        );
+        break;
+      }
+      messageGenerator.generateCommitMessage(args[1], args[2])
+        .then(commitMessage => {
+          message.channel.send(commitMessage);
+        })
+        .catch(_ => {
+          message.channel.send(_);
+        });
+      break;
+
+    default:
+      // Help
+      message.channel.send(
+        new Discord.RichEmbed()
+          .setDescription('Unrecognized parameter try using:')
+          .addField('pr <repo>', 'View pull requests')
+          .addField('leaderboard <repo>', 'Top 5 contributors')
+          .addField(
+            'commits <repo> <num>',
+            'Merged commits - <num> (optional, Limit: 25)'
+          )
+      );
+    }
+  },
+});
+
---
+++ src/util/GithubFetcher.js
@@ -0,0 +1,88 @@
+const fetch = require('node-fetch');
+const { SCE_GITHUB_API_URL } = require('../../config.json');
+
+/**
+ * Class that communicates with SCE server's Github API to
+ * retrieve data from Github
+ */
+class GithubFetcher {
+  /**
+   * Creates GithubFetcher object
+   */
+  constructor() {
+
+  }
+
+  /**
+   * This function retrieves a list of users that have contributed to a
+   * defined repo in the past month
+   * @param {*} repo Name of SCE repository
+   * @returns {Promise} Contributors
+   */
+  fetchLeaderboard(repo) {
+    return new Promise((resolve, reject) => {
+      fetch(`${SCE_GITHUB_API_URL}/api/routes/` +
+      `getContributorsInPastMonthFromRepo?repository=${repo}`
+      )
+        .then(data => data.json())
+        .then(object => {
+          resolve(object.contributors);
+        })
+        .catch(_ => {
+          reject(_);
+        });
+    });
+  }
+
+  /**
+   * This function retrieves a list of active pull requests from
+   * the defined repo
+   * @param {String} repo Name of SCE repository
+   * @returns {Promise} Pull Requests LIMIT OF 25
+   */
+  fetchPullRequests(repo) {
+    return new Promise((resolve, reject) => {
+      fetch(`${SCE_GITHUB_API_URL}/api/routes/` +
+        `getPullRequestsFromRepo?repository=${repo}`
+      )
+        .then(data => data.json())
+        .then(object => {
+          const pullRequests = object.pullRequests.map(pr => ({
+            name: `#${pr.number} ${pr.title}`,
+            value: `[view](${pr.pullRequestUrl})`
+          }));
+          resolve(pullRequests.slice(0, 25));
+        })
+        .catch(_ => {
+          reject(_);
+        });
+    });
+  }
+
+  /**
+   * This function retrieves a list of commits from the
+   * the defined repo
+   * @param {String} repo Name of SCE repository
+   * @returns {Promise} Commits
+   */
+  fetchCommits(repo) {
+    return new Promise((resolve, reject) => {
+      fetch(`${SCE_GITHUB_API_URL}/api/routes/` +
+        `getCommitsFromRepo?repository=${repo}`
+      )
+        .then(data => data.json())
+        .then(object => {
+          const commits = object.commits.map(commit => ({
+            name: `#${commit.user}`,
+            value: `[${commit.message}](${commit.commitUrl})`
+          }));
+          resolve(commits);
+        })
+        .catch(_ => {
+          reject(_);
+        });
+    });
+  }
+}
+
+module.exports = { GithubFetcher };
