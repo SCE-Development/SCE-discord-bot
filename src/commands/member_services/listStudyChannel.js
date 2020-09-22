280caefd9499f7a59485aa8825293fcee14bbcf8
Habib <has.sorathia@gmail.com>
2020-09-22 01:00:31 -0700

    linter hates me

--- src/commands/member_services/listStudyChannel.js
+++ src/commands/member_services/listStudyChannel.js
@@ -8,31 +8,31 @@ module.exports = new Command({
   aliases: ['lsc'],
   permissions: 'member',
   example: 's!lsc',
+  // eslint-disable-next-line
   execute: async (message, args) => {
-    const author = message.member;
     const { channels } = message.guild;
 
     const studyChannels = channels.array().filter(
-        (x) => x.type == 'category' && x.name == 'study'
+      (x) => x.type == 'category' && x.name == 'study'
     );
     if (studyChannels.length == 0) {
-        message.channel.send('Create a study category first!');
-        return;
+      message.channel.send('Create a study category first!');
+      return;
     }
     if (studyChannels.length > 1) {
-        message.channel.send('Ambiguous command. Delete a study category first');
-        return;
+      message.channel.send('Ambiguous command. Delete a study category first');
+      return;
     }
     let studyChannel = studyChannels[0];
     let textChannels = channels.array().filter(
-        (x) => (x.type == 'text' && x.parentID == studyChannel.id)
+      (x) => (x.type == 'text' && x.parentID == studyChannel.id)
     );
     
     textChannels = textChannels.map((channel) => '`'+channel.name+'`');
     const listEmbed = new Discord.RichEmbed()
-        .setTitle("List all the study channels")
-        .setColor('#ccffff')
-        .setDescription(textChannels.join("\n"));
+      .setTitle('List all the study channels')
+      .setColor('#ccffff')
+      .setDescription(textChannels.join('\n'));
 
     message.channel.send(listEmbed);
   },
