83416801d63e937f98383425b454f3123f127462
Jerry Lee <jerrylee1157@gmail.com>
2020-09-29 17:31:51 -0700

    Ignore ssh key

--- api/models/points.js
+++ api/models/points.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const PointSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    username: String,
-    userID: String,
-    points: Number,
-    lastTalked: String
+  _id: mongoose.Schema.Types.ObjectId,
+  username: String,
+  userID: String,
+  points: Number,
+  lastTalked: String
 });
 
 module.exports = mongoose.model('Points', PointSchema);
