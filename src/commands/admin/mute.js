b621800f68520e479a2d08f46b7cb531430d5922
Sam Low <slowc4g@gmail.com>
2020-10-07 22:12:51 -0700

    Role now added and applied to user, need to apply perms to all channels.

--- src/commands/admin/mute.js
+++ src/commands/admin/mute.js
@@ -8,31 +8,35 @@ module.exports = new Command({
   category: 'Server management',
   aliases: [],
   permissions: 'admin',
-  execute: (message, args) => {
+  execute: async (message, args) => {
     const author = message.member;
-    // Check if author can warn
+    const user = message.guild.member(args[0].match(/(\d+)/)[0]);
+    // Check if author can mute
     if (!isOfficer(author)) {
-      // something about cannot mute
       message.channel.send(author + ', you do not have that permission!');
+      return;
+    }
+    // Officers cannot mute other officers
+    if (isOfficer(user)) {
+      message.channel.send(author + ', you cannot mute other officers!');
+      return;
     }
     if (args.join(' ') == '') {
       message.channel.send('You need to give a user to mute.');
       // or set perms
       return;
     }
-    const user = message.guild.member(args[0].match(/(\d+)/)[0]);
-    const roles = message.guild;
+    const { channels, roles } = message.guild;
     let reason = args.slice(1).join(' ');
-    // check if muted role exists
+    // Check if muted role exists
     let mutedRole = roles.array().filter(
       (x) => x.name == "Muted"
     );
     let targetRole;
-    // if role does not exist, create role
+    // If role does not exist, create role
     if (mutedRole.length > 0) {
       targetRole = mutedRole[0];
-      await targetRole.setPermissions(0)
-        .then(message.channel.send('Reset role permissions'));
+      await targetRole.setPermissions(0);
     } else {
       await message.guild.createRole(
         {
@@ -40,12 +44,42 @@ module.exports = new Command({
           permissions: 0
         }
       )
-      .then((res) => {
-        targetRole = res;
-      });
+        .then((res) => {
+          targetRole = res;
+        });
     }
-    // set permissions: cannot speak in any channel
-    // apply to user
-    // if user has muted role, remove role
+    // Set permissions: cannot speak in any channel
+    const replacementPerms = {
+      id: targetRole.id,
+      deny: 2048
+    }
+
+    await message.channel.replacePermissionOverwrites({ // .guild doesn't work, .channel does
+      overwrites: [replacementPerms],
+    })
+      .then(() => {
+        if (user.roles.array().map((x) => x.name).includes("Muted")) {
+          user.removeRole(targetRole)
+            .then(() =>
+              message.channel.send(user + ' has been unmuted.')
+            );
+        }
+        else {
+          user.addRole(targetRole)
+            .then(() => {
+              if (reason) {
+                message.channel.send(user + ' has been muted for ' + reason + '.');
+                message.user.send('You have been muted for ' + reason + '.')
+              } else {
+                message.channel.send(user + ' has been muted.');
+                message.user.send('You have been muted. No reason was given.')
+              }
+            }
+            );
+        }
+      })
+      .catch(() => {
+        message.channel.send('There was an error in editing permissions!')
+      });
   }
 });
