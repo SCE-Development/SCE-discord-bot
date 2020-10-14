d044a8dc921f5e71db1d37296de441f5de13068c
Jerry Lee <jerrylee1157@gmail.com>
2020-10-14 16:11:00 -0700

    Set up backend / refactor server.js (#78)
    * Sort lsc alphabetically (#71)
    
    * Creation of study channel now checks against a regex expression to make sure it's a CMPE/CS course
    You can override this using force
    
    * Fixed variable names
    
    * Added backend
    
    * Ignore ssh key
    
    * Added root query
    
    * Set up graphQL backend
    
    * Rebase with master and modify server
    
    * Fixed server

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const Discord = require('discord.js');
-const { prefix, API_TOKEN } = require('./config.json');
+const { prefix, API_TOKEN, database } = require('./config.json');
 const { CommandHandler } = require('./src/CommandHandler');
-const { 
-  VoiceChannelChangeHandler 
+const {
+  VoiceChannelChangeHandler
 } = require('./src/VoiceChannelChangeHandler');
+const mongoose = require('mongoose');
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { schema } = require('./api/resolvers/index.js');
+const port = 5000;
 
 const startBot = async () => {
   const client = new Discord.Client();
@@ -12,7 +17,7 @@ const startBot = async () => {
   client.once('ready', () => {
     commandHandler.initialize();
     client.user.setActivity('Managing the SCE');
-    console.log('Ready');
+    console.log('Discord bot live');
   });
 
   client.on('message', message => {
@@ -26,4 +31,39 @@ const startBot = async () => {
   client.login(API_TOKEN);
 };
 
+// Conenct to mongoose
+const startDatabase = () => {
+  const url = `mongodb://localhost/${database}`;
+  mongoose.connect(url, {
+    autoIndex: true,
+    poolSize: 50,
+    bufferMaxEntries: 0,
+    keepAlive: 120,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  });
+  mongoose.connection.once('open', () =>
+    console.log('Connected to Mongo')
+  );
+};
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    schema,
+    playground: true,
+    introspection: true
+  });
+  // Express app
+  const app = express();
+
+  server.applyMiddleware({ app });
+
+  // start app
+  app.listen(port, () => console.log(`Server running at port ${port}`));
+};
+
+
 startBot();
+startDatabase();
+startServer();
