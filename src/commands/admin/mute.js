b5640fd9e5236326c312d67c843c49d585f6ea06
Sam Low <66745211+qsaml@users.noreply.github.com>
2020-10-14 18:04:55 -0700

    Mute Command (#87)
    * Rough draft of creating muted role
    
    * Role now added and applied to user, need to apply perms to all channels.
    
    * All works together, just unsure why the catch statement at the end triggers.
    
    * --no-amend
    
    * --no-amend
    
    * Changed mute message to include invalid user
    
    * Shouldn't be repetitive anymore

---
+++ src/commands/admin/mute.js
@@ -0,0 +1,84 @@
+const Command = require('../Command');
+const { isOfficer } = require('../../util/Permission');
+
+module.exports = new Command({
+  name: 'mute',
+  description: 'Mute someone, or unmutes if the person is muted.',
+  category: 'Server management',
+  aliases: [],
+  permissions: 'admin',
+  execute: async (message, args) => {
+    const author = message.member;
+    // Check if author can mute
+    if (!isOfficer(author)) {
+      message.channel.send(author + ', you do not have that permission!');
+      return;
+    }
+    // Must mention a user in the server to mute them
+    if (args.join(' ') == '') {
+      message.channel.send('You need to mention a user to mute!');
+      return;
+    }
+    const user = message.guild.member(
+      args[0].match(/(\d+)/)
+      && args[0].match(/(\d+)/)[0]);
+    if (!user) {
+      message.channel.send('You need to mention a valid user to mute!');
+      return;
+    }
+    // Officers cannot mute other officers
+    if (isOfficer(user)) {
+      message.channel.send(author + ', you cannot mute other officers!');
+      return;
+    }
+    const { roles } = message.guild;
+    let reason = args.slice(1).join(' ');
+    // Check if muted role exists
+    let mutedRole = roles.array().filter(
+      (x) => x.name == 'Muted'
+    );
+    let targetRole;
+    // If role does not exist, create role
+    if (mutedRole.length > 0) {
+      targetRole = mutedRole[0];
+      await targetRole.setPermissions(0);
+    } else {
+      await message.guild.createRole(
+        {
+          name: 'Muted',
+          permissions: 0
+        }
+      )
+        .then((res) => {
+          targetRole = res;
+        });
+    }
+    // Changes the permissions for every channel
+    for (let i = 0; i < message.guild.channels.array().length; i++) {
+      await message.guild.channels.array()[i].overwritePermissions(targetRole, {
+        SEND_MESSAGES: false,
+        ADD_REACTIONS: false
+      });
+    }
+    // If user has muterole, remove it; if not, add it
+    if (user.roles.array().map((x) => x.name).includes('Muted')) {
+      await user.removeRole(targetRole)
+        .then(() =>
+          message.channel.send(user + ' **unmuted**.')
+        );
+    }
+    else {
+      await user.addRole(targetRole)
+        .then(() => {
+          if (reason) {
+            message.channel.send(user + ' was muted, **' + reason + '**');
+            user.send('You were muted, **' + reason + '**');
+          } else {
+            message.channel.send(user + ' has been **muted**.');
+            user.send('You have been muted. No reason was given.');
+          }
+          return;
+        });
+    }
+  }
+});
--- src/commands/admin/warn.js
+++ src/commands/admin/warn.js
@@ -3,13 +3,13 @@ const { isOfficer } = require('../../util/Permission');
 
 module.exports = new Command({
   name: 'warn',
-  description: 'Warn someone',
+  description: 'Warn someone.',
   category: 'Server management',
   aliases: [],
   permissions: 'admin',
   execute: (message, args) => {
     if (args.join(' ') == '') {
-      message.channel.send('You need to give a user to warn.');
+      message.channel.send('You need to mention a user to warn!');
       return;
     }
     const author = message.member;
@@ -17,13 +17,18 @@ module.exports = new Command({
     let reason = args.slice(1).join(' ');
     // Check if author can warn
     if (isOfficer(author)) {
+      // User must be in the server to warn
+      if (!message.guild.member.id) {
+        message.channel.send('That user doesn\'t exist!');
+        return;
+      }
       if (reason) {
-        user.send('You have been warned because ' + reason + '.');
-        message.channel.send(user + ' has been warned because '
-          + reason + '.');
+        user.send('You were warned, **' + reason + '**');
+        message.channel.send(user + ' warned: ** '
+          + reason + '**');
       } else {
-        user.send('You have been warned.');
-        message.channel.send(user + ' has been warned.');
+        user.send('You have been warned. No reason was given.');
+        message.channel.send(user + ' has been **warned**.');
       }
     } else {
       message.channel.send(author
