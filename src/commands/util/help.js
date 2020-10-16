7f978f2f7a394b3446e6f496ce80ab0154f5e19a
Davin Wong <54650468+RTX-Banana@users.noreply.github.com>
2020-10-16 11:28:27 -0700

    Cmdr (#89)
    * Refractored help command
    
    * Remove Comment
    
    * eslint

--- src/commands/util/help.js
+++ src/commands/util/help.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { isOfficer } = require('../../util/Permission');
 const Command = require('../Command');
 
 module.exports = new Command({
@@ -10,11 +11,19 @@ module.exports = new Command({
   permissions: 'general',
   execute: (message, args) => {
 
+    const author = message.member;
+
     const capitalize = (message) => (message.charAt(0).toUpperCase()
       + message.slice(1));
     const { commands } = message.client;
+
     // list of categories
-    const categories = (commands.map(command => command.category))
+    const categories = (commands.map((command) => {
+      if (command.permissions === 'admin' && !isOfficer(author)) {
+        return;
+      }
+      return command.category;
+    }))
       .filter((x, i, a) => a.indexOf(x) == i);
     // dictionary of categories
     let dict = {};
@@ -24,11 +33,17 @@ module.exports = new Command({
         .setColor('#ccffff')
         .setTitle('All commands');
       for (let i = 0; i < categories.length; i++) {
-        dict[categories[i]] = [];
+        if (categories[i]) {
+          dict[categories[i]] = [];
+        }
       }
       // tmp is all the unique commands
       let tmp = Array.from(new Set(commands.array()));
       for (let j = 0; j < tmp.length; j++) {
+        if (tmp[j].permissions === 'admin' && !isOfficer(author)) {
+          continue;
+        }
+
         dict[tmp[j].category].push(`\`${tmp[j].name}\``);
       }
 
@@ -40,15 +55,25 @@ module.exports = new Command({
       message.channel.send(helpEmbed);
     } else {
       let command = args[0].toLowerCase();
-      const helpEmbed = new Discord.RichEmbed()
-        .setColor('#ccffff')
-        .setTitle(capitalize(command));
+      
       // Grab command info
       const commandInfo = commands.get(command);
+      
       if (!commandInfo){
         message.channel.send(`Command ${command} does not exist!`);
         return;
       }
+
+      if (commandInfo.permissions === 'admin' && !isOfficer(author)) {
+        message.channel
+          .send('You do not have permissions to use this command!');
+        return;
+      }
+
+      const helpEmbed = new Discord.RichEmbed()
+        .setColor('#ccffff')
+        .setTitle(capitalize(command));
+      
       Object.entries(commandInfo).forEach(([field, info]) => {
         if (field == 'executeCommand' || !info || info.length == 0) return;
         let infoText = info;
