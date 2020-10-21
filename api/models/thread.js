ed27aecbf26191df6b6a4a69368957d4cb05d67a
Jerry Lee <jerrylee1157@gmail.com>
2020-10-20 20:22:22 -0700

    added thread database schemas

---
+++ api/models/thread.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const { composeMongoose } = require('graphql-compose-mongoose');
+const { ThreadMessageTC } = require('./threadMessage');
+
+
+const ThreadSchema = mongoose.Schema(
+  {
+    threadID: {
+      // Automatically assign using timestamp
+      type: String,
+      required: true,
+      unique: true
+    },
+    creatorID: {
+      type: String,
+      required: true
+    },
+    guildID: {
+      type: String,
+      required: true
+    },
+    topic: {
+      type: String,
+    },
+    threadMessages: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'ThreadMessage'
+      }
+    ]
+  },
+  { collection: 'threads' }
+);
+
+const Thread = mongoose.model('Thread', ThreadSchema);
+const ThreadTC = composeMongoose(Thread);
+
+// Add relations
+ThreadTC.addRelation(
+  'messages',
+  {
+    resolver: () => ThreadMessageTC.mongooseResolvers.dataLoaderMany(),
+    prepareArgs: {
+      _ids: (source) => source.threadMessages || []
+    },
+    projection: { threadMessages: true }
+  }
+)
+
+module.exports = {
+  Thread,
+  ThreadSchema,
+  ThreadTC,
+};
--- api/resolvers/index.js
+++ api/resolvers/index.js
@@ -1,14 +1,21 @@
 const { schemaComposer } = require('graphql-compose');
-const { PointQuery, PointMutation } = require('../resolvers/points');
-
-
+const { PointQuery, PointMutation } = require('./points');
+const { ThreadQuery, ThreadMutation } = require('./thread');
+const {
+  ThreadMessageQuery,
+  ThreadMessageMutation
+} = require('./threadMessage');
 
 schemaComposer.Query.addFields({
   ...PointQuery,
+  ...ThreadQuery,
+  ...ThreadMessageQuery,
 });
 
 schemaComposer.Mutation.addFields({
   ...PointMutation,
+  ...ThreadMutation,
+  ...ThreadMessageMutation,
 });
 
 const schema = schemaComposer.buildSchema();
