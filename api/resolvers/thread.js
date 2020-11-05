344a41ea5235cda1cece9a98aa8e5ad5aca0ab0e
Jerry Lee <jerrylee1157@gmail.com>
2020-11-05 02:03:39 -0800

    Added backend graphql thread

--- api/resolvers/thread.js
+++ api/resolvers/thread.js
@@ -1,26 +1,72 @@
 const {
-  ThreadTC
+  ThreadTC,
+  Thread,
 } = require('../models/thread');
+const { ThreadMessage } = require('../models/threadMessage');
 
 const ThreadQuery = {
-  threadById: ThreadTC.mongooseResolvers.findById(),
-  threadByIds: ThreadTC.mongooseResolvers.findByIds(),
   threadOne: ThreadTC.mongooseResolvers.findOne(),
   threadMany: ThreadTC.mongooseResolvers.findMany(),
   threadCount: ThreadTC.mongooseResolvers.count(),
-  threadConnection: ThreadTC.mongooseResolvers.connection(),
-  threadPagination: ThreadTC.mongooseResolvers.pagination(),
 };
 
 const ThreadMutation = {
   threadCreateOne: ThreadTC.mongooseResolvers.createOne(),
-  threadCreateMany: ThreadTC.mongooseResolvers.createMany(),
-  threadUpdateById: ThreadTC.mongooseResolvers.updateById(),
   threadUpdateOne: ThreadTC.mongooseResolvers.updateOne(),
-  threadUpdateMany: ThreadTC.mongooseResolvers.updateMany(),
-  threadRemoveById: ThreadTC.mongooseResolvers.removeById(),
   threadRemoveOne: ThreadTC.mongooseResolvers.removeOne(),
   threadRemoveMany: ThreadTC.mongooseResolvers.removeMany(),
+  threadAddMessage: {
+    type: ThreadTC,
+    args: { threadID: 'String!', messageID: 'String!' },
+    resolve: async (source, args) => {
+      const message = await ThreadMessage.create({
+        messageID: args.messageID
+      });
+      // error
+      if (!message) return null;
+
+      const thread = await Thread.findOneAndUpdate(
+        { threadID: args.threadID },
+        { $addToSet: { threadMessages: message } },
+        { new: true }
+      );
+      return thread;
+    }
+  },
+  threadCreate: {
+    type: ThreadTC,
+    args: {
+      threadID: 'String!',
+      creatorID: 'String!',
+      guildID: 'String!',
+      topic: 'String',
+      messageID: 'String!',
+    },
+    resolve: async (source, args) => {
+      const {
+        threadID,
+        creatorID,
+        guildID,
+        topic,
+        messageID,
+      } = args;
+      const message = await ThreadMessage.create({
+        messageID
+      });
+      // error
+      if (!message) return null;
+
+      const thread = await Thread.create({
+        threadID,
+        creatorID,
+        guildID,
+        topic,
+        threadMessages: [message]
+      });
+      if (!thread) return null;
+      return thread;
+    }
+  }
 };
 
 module.exports = { ThreadQuery, ThreadMutation };
