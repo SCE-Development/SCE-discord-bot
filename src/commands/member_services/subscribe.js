a203419ac327dbfa91fa31518a11854e7027fd29
Rakan Kandah <rakankandah@gmail.com>
2020-11-13 13:37:50 -0800

    changed formatting

--- src/commands/member_services/subscribe.js
+++ src/commands/member_services/subscribe.js
@@ -1,27 +1,29 @@
 const Command = require('../Command');
 module.exports = new Command({
-    name: 'subscribe',
-    aliases:['sub'],
-    description: 'subscribes user to discord SCE notifications',
-    example: '!subscribe',
-    permissions:'general',
-    category: 'member services',
-    execute: async (message, args) => {
-        const author = message.member; 
-        const { roles } = message.guild; 
+  name: 'subscribe',
+  aliases:['sub'],
+  description: 'subscribes user to discord SCE notifications',
+  example: '!subscribe',
+  permissions:'general',
+  category: 'member services',
+  // eslint-disable-next-line
+  execute: async (message, args) => {
+    const author = message.member; 
+    const { roles } = message.guild; 
         
-        let classRole = roles.array().filter(
-          (x) => x.name == "Notification"
-        );
-        if(classRole.length ==0){
-            message.channel.send("The Notification role has not been setup");
-            return;
-        }   
-        let targetRole = classRole[0];
-        await author.addRole(targetRole)
-        .then(() =>
-          message.channel.send(author + ` has subscribed to all SCE Discord Notifications`))
-        .catch(() =>
-          message.channel.send("Could not add role"));
-    }
+    let classRole = roles.array().filter(
+      (x) => x.name == 'Notification'
+    );
+    if(classRole.length ==0){
+      message.channel.send('The Notification role has not been setup');
+      return;
+    }   
+    let targetRole = classRole[0];
+    await author.addRole(targetRole)
+      .then(() =>
+        message.channel.send(author +
+           ' has subscribed to all SCE Discord Notifications'))
+      .catch(() =>
+        message.channel.send('Could not add role'));
+  }
 });
--- src/commands/member_services/unsubscribe.js
+++ src/commands/member_services/unsubscribe.js
@@ -1,27 +1,29 @@
 const Command = require('../Command');
 module.exports = new Command({
-    name: 'unsubscribe',
-    aliases:['unsub'],
-    description: 'unsubscribes user from discord SCE notifications',
-    example: '!unsubscribe',
-    permissions:'general',
-    category: 'member services',
-    execute: async (message, args) => {
-        const author = message.member; 
-        const { roles } = message.guild; 
+  name: 'unsubscribe',
+  aliases:['unsub'],
+  description: 'unsubscribes user from discord SCE notifications',
+  example: '!unsubscribe',
+  permissions:'general',
+  category: 'member services',
+  // eslint-disable-next-line
+  execute: async (message, args) => {
+    const author = message.member; 
+    const { roles } = message.guild; 
         
-        let classRole = roles.array().filter(
-          (x) => x.name == "Notification"
-        );
-        if(classRole.length ==0){
-            message.channel.send("The Notification role has not been setup");
-            return;
-        }   
-        let targetRole = classRole[0];
-        await author.removeRole(targetRole)
-        .then(() =>
-          message.channel.send(author + ` has unsubscribed from all SCE Discord Notifications`))
-        .catch(() =>
-          message.channel.send("Could not remove role"));
-    }
+    let classRole = roles.array().filter(
+      (x) => x.name == 'Notification'
+    );
+    if(classRole.length ==0){
+      message.channel.send('The Notification role has not been setup');
+      return;
+    }   
+    let targetRole = classRole[0];
+    await author.removeRole(targetRole)
+      .then(() =>
+        message.channel.send(author + 
+          ' has unsubscribed from all SCE Discord Notifications'))
+      .catch(() =>
+        message.channel.send('Could not remove role'));
+  }
 });
