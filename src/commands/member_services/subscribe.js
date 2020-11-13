34588aa39010bdf015e64e69eeeaa9456ddc8ca2
Rakan Kandah <rakankandah@gmail.com>
2020-11-13 13:27:26 -0800

    SUB AND UNSUB BOT CREATED

---
+++ src/commands/member_services/subscribe.js
@@ -0,0 +1,27 @@
+const Command = require('../Command');
+module.exports = new Command({
+    name: 'subscribe',
+    aliases:['sub'],
+    description: 'subscribes user to discord SCE notifications',
+    example: '!subscribe',
+    permissions:'general',
+    category: 'member services',
+    execute: async (message, args) => {
+        const author = message.member; 
+        const { roles } = message.guild; 
+        
+        let classRole = roles.array().filter(
+          (x) => x.name == "Notification"
+        );
+        if(classRole.length ==0){
+            message.channel.send("The Notification role has not been setup");
+            return;
+        }   
+        let targetRole = classRole[0];
+        await author.addRole(targetRole)
+        .then(() =>
+          message.channel.send(author + ` has subscribed to all SCE Discord Notifications`))
+        .catch(() =>
+          message.channel.send("Could not add role"));
+    }
+});
