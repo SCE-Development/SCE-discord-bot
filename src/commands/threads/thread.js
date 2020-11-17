5a4fed5cddbce5f43da8020cc19c4a20c362271a
charlotte-zhuang <charlotte.zhuang@sjsu.edu>
2020-11-17 00:59:42 -0500

    Implemented thread and threadmanager commands with dummy requests.

--- src/commands/threads/thread.js
+++ src/commands/threads/thread.js
@@ -1,13 +1,90 @@
-
+const Discord = require('discord.js');
 const Command = require('../Command');
 
 module.exports = new Command({
   name: 'thread',
-  description: 'Create a new thread with a message',
+  description: 'View active threads or start a new one',
   category: 'custom threads',
   aliases: [],
   permissions: 'general',
+  params: 'none (view threads), <topic> (start new thread)',
+  example: 's!thread <topic (optional)>',
   execute: (message, args) => {
-    console.log('Executed Command: <thread>, args=' + args);
+    message.content = args.join(' ').trim();
+
+    if (message.content.length === 0) {
+      // show all active threads
+      const CURRENT_DATE = new Date();
+      const MS_PER_DAY = 1000 * 60 * 60 * 24;
+      const checkIfActive = (message) =>
+        (CURRENT_DATE - message.createdAt) / MS_PER_DAY < 7;
+      const requestMessage = (id) => message.channel
+        .fetchMessage(id)
+        .then( (response) => response );
+      // TODO implement query
+      const response = {responseData: [
+        {
+          threadID: 1000,
+          topic: 'Test Message',
+          threadMessages: [{ messageID: '778096468958380042' }]
+        },
+        {
+          threadID: 1001,
+          topic: null,
+          threadMessages: [{ messageID: '778127742109483008' }]
+        },
+        {
+          threadID: 1002,
+          topic: 'Old Message',
+          threadMessages: [{ messageID: '770856956449652776' }]
+        }
+      ]};
+
+      const makeEmbed = async () => {
+        const embed = new Discord.RichEmbed()
+          .setTitle('Active Threads')
+          .setDescription(
+            'All threads in this channel with activity in the past week'
+          );
+
+        for (let i = 0; i < response.responseData.length; i++) {
+          const thread = response.responseData[i];
+
+          let lastMessage = await requestMessage(
+            thread.threadMessages[thread.threadMessages.length - 1].messageID
+          );
+        
+          // do not display messages older than a week old
+          if (!checkIfActive(lastMessage)) {
+            break;
+          }
+          // add the thread and display the last message
+          if (thread.topic) {
+            embed.addField(
+              thread.topic + ' (id: ' + thread.threadID + ')',
+              lastMessage.content
+            );
+          } else {
+            embed.addField(
+              ' (id: ' + thread.threadID + ')',
+              lastMessage.content
+            );
+          }
+        }
+
+        return embed;
+      };
+
+      makeEmbed().then( (embed) => message.channel.send(embed) );
+    } else {
+      // start new thread
+      // TODO implement query
+      const response = { data: { threadID: 1000 } };
+      const threadEmbed = new Discord.RichEmbed()
+        .setTitle('New Thread')
+        .addField('ID', response.data.threadID)
+        .addField('Topic', message.content);
+      message.channel.send(threadEmbed);
+    }
   }
 });
--- src/commands/threads/threadManager.js
+++ src/commands/threads/threadManager.js
@@ -1,12 +1,84 @@
+const Discord = require('discord.js');
+const { isOfficer } = require('../../util/Permission');
 const Command = require('../Command');
 
 module.exports = new Command({
   name: 'threadmanager',
-  description: 'Thread Manager. Used for managing custom threads.',
+  description: 'Create and remove threads.',
   category: 'custom threads',
   aliases: ['tm'],
   permissions: 'admin',
+  params: '`create`, `remove`',
+  example: 's!tm <command> <param>',
   execute: (message, args) => {
-    console.log('Executed Command: <thread manager>, args=' + args);
+    const author = message.member;
+    // Check for author permissions
+    if (!isOfficer(author)) {
+      message.channel.send('You do not have sufficient permissions!');
+      return;
+    }
+    
+    switch(args[0]) {
+      case 'create': {
+        // Create a thread
+        const topic = args.slice(1).join(' ');
+        // TODO implement mutation
+        const response = { data: { threadID: 1000 } };
+        const threadEmbed = new Discord.RichEmbed()
+          .setTitle('Created new thread')
+          .addField('id', response.data.threadID);
+        // check if there's a topic
+        if (topic.length > 0) {
+          threadEmbed.addField('topic', topic);
+        }
+        message.channel.send(threadEmbed);
+        break;
+      }
+      case 'remove':
+      case 'rm': {
+        // check if a thread id was supplied
+        if (args.length < 2) {
+          message.channel.send(new Discord.RichEmbed()
+            .setColor('#ccffff')
+            .setTitle('Remove Thread')
+            .addField('s!tm remove <thread id>', 'Removes a thread')
+            .addField('Aliases', 'rm'));
+          return;
+        }
+        const id = args.slice(1).join('').replace(/\s+/, '');
+        if (!/^\d{1,18}/.test(id)) {
+          message.channel.send(
+            'Could not remove thread '+
+            id +
+            '. ID should be a number up to 18 digits.'
+          );
+          return;
+        }
+        // TODO implment mutation
+        const response = { error: true };
+        if (response.error) {
+          message.channel.send(
+            'Error removing thread ' + id + '. Perhaps there is a typo?'
+          );
+        } else {
+          message.channel.send('Removed thread ' + id);
+        }
+        break;
+      }
+
+      default: {
+        // Help
+        message.channel.send(
+          new Discord.RichEmbed()
+            .setColor('#ccffff')
+            .setTitle('Thread Manager')
+            .setDescription('Unrecognized command, try using:')
+            .addField('s!tm create <topic>',
+              'Creates a new thread - <topic> (optional)')
+            .addField('s!tm remove <thread ID>',
+              'Removes a thread - alias (rm)')
+        );
+      }
+    }
   }
 });
