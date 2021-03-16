b9c6c32555e56b766f100fd504794d0b32cc46c9
charlotte-zhuang <charlotte.zhuang@sjsu.edu>
2021-03-16 01:35:47 -0400

    revert initdb

--- mongo/init-mongo.sh
+++ mongo/init-mongo.sh
@@ -2,13 +2,13 @@
 set -e
 
 mongo <<EOF
-use $MONGO_DATABASE
+use $MONGO_INITDB_DATABASE
 db.createUser({
   user: '$MONGO_BOT_USER',
   pwd: '$MONGO_BOT_PASSWORD',
   roles: [{
     role: 'readWrite',
-    db: '$MONGO_DATABASE'
+    db: '$MONGO_INITDB_DATABASE'
   }]
 })
 EOF
