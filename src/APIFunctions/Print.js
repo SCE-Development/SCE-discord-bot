52164494479c0442caa4038817dc8548625cf8a3
Truong Phu Vu <truongphu.vu@sjsu.edu>
2022-07-18 03:02:59 -0700

    created api function for editPrintedPages, update documentation

--- src/APIFunctions/Print.js
+++ src/APIFunctions/Print.js
@@ -1,15 +1,27 @@
 const axios = require('axios');
 const { CORE_V4_API_KEY } = require('../../config.json');
 
-let MAINENDPOINTS_API_URL = 'http://localhost:8080/mainendpoints/User';
+let MAINENDPOINTS_API_URL = 'http://localhost:8080/api/user';
 let PERIPHERAL_API_URL = 'http://localhost:8081/peripheralapi';
 
+/**
+ * @summary Calling getUserFromDiscordId endpoint from 
+ * Core-V4/main_endpoints/User.js to check if user's Discord ID is in database.
+ * @param {Number} discordID
+ * @return {Promise} A promise that contains a boolean variable of 
+ * user's discord id validation and number of printed pages
+ */
 function validateDiscordID(discordID) {
-  return new Promise((resolve) => {
+  return new Promise(( resolve ) => {
     // eslint-disable-next-line max-len
-    axios.get(`${MAINENDPOINTS_API_URL}/getUserFromDiscordId?discordID=${discordID}&apiKey=${CORE_V4_API_KEY}`)
-      .then(() => {
-        resolve(true);
+    axios.post(`${MAINENDPOINTS_API_URL}/getUserFromDiscordId`, {discordID, apiKey: CORE_V4_API_KEY})
+      .then((res) => {
+        const {pagesPrinted} = res.data;
+        const result = {
+          isValid: true, 
+          pagesPrinted: pagesPrinted
+        };
+        res.status === 200 && resolve(result);
       })
       .catch(() => {
         resolve(false);
@@ -17,8 +29,15 @@ function validateDiscordID(discordID) {
   });
 }
 
+/**
+ * @summary Calling healthCheck endpoint from Core-V4/peripheral/Printer.js
+ * to check if the printer is in service.
+ * @return {Promise} A promise that will return true if printer is in service
+ * and vice versa
+ * 
+ */
 function printerHealthCheck() {
-  return new Promise((resolve) => {
+  return new Promise(( resolve ) => {
     axios.get(`${PERIPHERAL_API_URL}/Printer/healthCheck`)
       .then(() => {
         resolve(true);
@@ -29,10 +48,44 @@ function printerHealthCheck() {
   });
 }
 
+/**
+ * @summary Calling pushDiscordPDFToSqs endpoint from 
+ * Core-V4/peripheral/Printer.js to push the PDF's url to SQS
+ * @param {Number} fileURL
+ * @return {Promise} A promise.
+ * 
+ */
 function pushDiscordPDFToSqs(fileURL) {
-  return new Promise((resolve) => {
+  return new Promise(( resolve ) => {
     axios.post(`${PERIPHERAL_API_URL}/Printer/pushDiscordPDFToSqs`,
-      { fileURL, CORE_V4_API_KEY })
+      { fileURL, apiKey: CORE_V4_API_KEY })
+      .then(() => {
+        resolve(true);
+      })
+      .catch(() => {
+        resolve(false);
+      });
+  });
+}
+
+/**
+ * @summary Calling updatePagesPrintedFromDiscord endpoint from 
+ * Core-V4/main_endpoint/User.js to edit user's printed pages
+ * @param {String} discordID
+ * @param {Number} printedPages
+ * @return {Promise} A promise that resolve true if printedPages 
+ * is successfully updated and vice versa.
+ * 
+ */
+function editUserPagesPrinted ( discordID, printedPages ) {
+  return new Promise(( resolve ) => {
+    const body = {
+      discordID: discordID,
+      apiKey: CORE_V4_API_KEY,
+      printedPages: printedPages
+    };
+    // eslint-disable-next-line max-len
+    axios.post(`${MAINENDPOINTS_API_URL}/updatePagesPrintedFromDiscord`, {...body})
       .then(() => {
         resolve(true);
       })
@@ -45,5 +98,6 @@ function pushDiscordPDFToSqs(fileURL) {
 module.exports = {
   validateDiscordID,
   printerHealthCheck,
-  pushDiscordPDFToSqs
+  pushDiscordPDFToSqs,
+  editUserPagesPrinted
 };
--- src/commands/member_services/print.js
+++ src/commands/member_services/print.js
@@ -20,7 +20,8 @@ module.exports = new Command({
     
     const reply = await message.channel.send('Authenticating Print Request.');
 
-    let isValid = await validateDiscordID( id );
+    // eslint-disable-next-line no-unused-vars
+    let {isValid, pagesPrinted} = await validateDiscordID( id );
 
     if (!isValid) {
       return reply.edit('Connect your discord account with' +
@@ -28,31 +29,45 @@ module.exports = new Command({
     }
 
     let url;
+    let timeout = false;
     reply.edit('Check your DM');
     await message.author.send('Please submit one file at a time!');
     const filter = collected => collected.author.id === message.author.id;
     await message.channel.awaitMessages(filter, {
       max: 1,
-      time: 15000
+      time: 10000
     })
       .then(collected => {
         url = collected.first().attachments.first().url;
       })
       .catch(() => {
-        return message.author.send('Timeout');
+        timeout = true;
       });
+    
+    if (!timeout){
+      let isPrinterWorking  = printerHealthCheck();
 
-    let isPrinterWorking  = printerHealthCheck();
-
-    if(!isPrinterWorking){
-      return reply.edit('Printer is out of service at the moment!' +
+      if(!isPrinterWorking){
+        return reply.edit('Printer is out of service at the moment!' +
         'Please try again later!');
-    }
-    const isPushDiscordPDFToSqs = pushDiscordPDFToSqs(url);
+      }
+      // get number of pages from user, already sent 
+      // via checkDiscordAPI endpoint
+      // calculate number of pages
+      // subtract and update page count of the user via User.edit api endpoint
 
-    if(!isPushDiscordPDFToSqs){
-      return reply.edit('Unable to print your file! Please try again!');
+      // 1. write api function for User.edit (done)
+      // 2. update checkDiscordAPI api function to return 
+      // the number of pages along with the boolean value (done)
+      // 3. update documetation
+      // 4. commit and push
+      const isPushDiscordPDFToSqs = pushDiscordPDFToSqs(url);
+
+      if(!isPushDiscordPDFToSqs){
+        return reply.edit('Unable to print your file! Please try again!');
+      }
+      return message.author.send('Printing...');
     }
-    return message.author.send('Printing...');
+    
   }
 });
