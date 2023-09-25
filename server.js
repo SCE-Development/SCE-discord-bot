0ef2b2452e4f7ddf1f37233d223861ca4d72e666
Dung Dong <dongtandung2001@gmail.com>
2023-09-25 16:48:29 -0700

    Simple reaction for role

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const {
 } = require('./src/handlers/VoiceChannelChangeHandler');
 const { NewMemberAddHandler } = require('./src/handlers/NewMemberAddHandler');
 
+
+
 const startBot = async () => {
   const client = new Discord.Client({
     intents: [
@@ -16,7 +18,9 @@ const startBot = async () => {
       Discord.GatewayIntentBits.GuildMessages,
       Discord.GatewayIntentBits.MessageContent,
       Discord.GatewayIntentBits.GuildVoiceStates,
-    ]
+      Discord.GatewayIntentBits.GuildMessageReactions,
+    ],
+    partials: [Discord.Partials.Channel, Discord.Partials.Message, Discord.Partials.Reaction],
   });
   const messageHandler = new MessageHandler(prefix);
   const vcChangeHandler = new VoiceChannelChangeHandler();
@@ -43,6 +47,29 @@ const startBot = async () => {
   client.on('guildMemberAdd', (newMember) => {
     newMemberHandler.handleNewMember(newMember);
   });
+
+  client.on('messageReactionAdd', async (reaction, user) => {
+    // can replace this msg id to a specfic msg to listen for reactions
+    if (reaction.message.id === '1155762142516097034') {
+      console.log("reaction added", user.id)
+      // get member
+      const member = reaction.message.guild.members.cache.get(user.id);
+      // can replace this id to a specific role id from discord server
+      const role = reaction.message.guild.roles.cache.get('1155733322064998424')
+      member.roles.add(role)
+    }
+  })
+
+  client.on('messageReactionRemove', async (reaction, user) => {
+    if (reaction.message.id === '1155762142516097034') {
+      console.log("reaction remove")
+      const member = reaction.message.guild.members.cache.get(user.id);
+      const role = reaction.message.guild.roles.cache.get('1155733322064998424')
+      member.roles.remove(role);
+    }
+  })
+
+
 
   client.login(API_TOKEN);
 };
