d54f956c932b68220cbd06ddffb251270c86ae1b
Dung Dong <40316606+dongtandung2001@users.noreply.github.com>
2023-09-28 00:55:03 -0700

    Setup for feature: reaction assign "verified" role (#174)
    * Simple reaction for role
    
    * Put msg id and role_id in config.js
    
    * npm lint
    
    * Update server.js
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    * Update config.example.json
    
    * Update server.js
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const Discord = require('discord.js');
 const {
   prefix,
   API_TOKEN,
+  VERIFICATION = {}
 } = require('./config.json');
 const { MessageHandler } = require('./src/handlers/MessageHandler');
 const {
   VoiceChannelChangeHandler,
 } = require('./src/handlers/VoiceChannelChangeHandler');
 const { NewMemberAddHandler } = require('./src/handlers/NewMemberAddHandler');
+
+
 
 const startBot = async () => {
   const client = new Discord.Client({
@@ -16,7 +19,13 @@ const startBot = async () => {
       Discord.GatewayIntentBits.GuildMessages,
       Discord.GatewayIntentBits.MessageContent,
       Discord.GatewayIntentBits.GuildVoiceStates,
-    ]
+      Discord.GatewayIntentBits.GuildMessageReactions,
+    ],
+    partials:
+      [
+        Discord.Partials.Channel,
+        Discord.Partials.Message,
+        Discord.Partials.Reaction],
   });
   const messageHandler = new MessageHandler(prefix);
   const vcChangeHandler = new VoiceChannelChangeHandler();
@@ -42,6 +51,19 @@ const startBot = async () => {
 
   client.on('guildMemberAdd', (newMember) => {
     newMemberHandler.handleNewMember(newMember);
+  });
+
+  client.on('messageReactionAdd', async (reaction, user) => {
+    // can replace this msg id to a specfic msg to listen for reactions
+    if (reaction.message.id === VERIFICATION.MESSAGE_ID) {
+      // get member
+      const member = reaction.message.guild.members.cache.get(user.id);
+      // can replace this id to a specific role id from discord server
+      const role = reaction.message.guild.roles.cache.get(
+        VERIFICATION.VERIFIED_ROLE_ID
+      );
+      member.roles.add(role);
+    }
   });
 
   client.login(API_TOKEN);
