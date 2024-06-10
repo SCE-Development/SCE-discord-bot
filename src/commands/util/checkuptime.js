cfb04e69c306ec66314872cdd0c65b98f4eefb87
Phong Diep <88680156+PhongDiep2003@users.noreply.github.com>
2024-06-10 15:16:43 -0700

    implemented the function for uptime command (#190)
    * implemented the function for uptime command
    
    * Fixed lint errors
    
    * Added tab after //
    
    * Added new line at the end of the file and reduced the length of line 23
    
    * Moved the function convertMSToISO above the line with module.export and added line break in the returned message

---
+++ src/commands/util/checkuptime.js
@@ -0,0 +1,48 @@
+const {prefix} = require('../../../config.json');
+const Command = require('../Command');
+
+const converMSToISO = (ms) => {
+  const milisecondPerMinute = 60000;
+  const milisecondPerHour = 60 * milisecondPerMinute;
+  const milisecondPerDay = 24 * milisecondPerHour;
+
+  const days = Math.floor(ms / milisecondPerDay);
+  ms %= milisecondPerDay;
+
+  const hours = Math.floor( ms / milisecondPerHour);
+  ms %= milisecondPerHour;
+
+  const minutes = Math.floor(ms / milisecondPerMinute);
+  ms %= milisecondPerMinute;
+
+  return {days, hours, minutes};
+};
+
+module.exports = new Command({
+  name: 'uptime',
+  description: 'Check the bot\'s uptime',
+  aliases: [],
+  example: `${prefix}uptime`,
+  permissions: 'general',
+  category: 'information',
+  execute: async (message) => {
+    //  send the botStartTime to the channel
+    const today = new Date();
+    //  get the bot start time from the message object
+    //  calculate the difference between the current time and the bot start time
+    const diffDate = today - message.botStartTime;
+    //  convert the difference to ISO format
+    const {days, hours, minutes} = converMSToISO(diffDate);
+    const returnedDay = `${days} ${days > 1 ? 'days' : 'day'}`;
+    const returnedHour = `${hours} ${hours > 1 ? 'hours' : 'hour'}`;
+    const returnedMinute = `${minutes} ${minutes > 1 ? 'minutes' : 'minute'}`;
+    const botStartTimeISO = message.botStartTime.toISOString();
+    const returnedMsg = `up ${returnedDay}, ` +
+                    `${returnedHour}, ` +
+                    `${returnedMinute} \n` +
+                    `since ${botStartTimeISO}`;
+    //  send the message to the channel
+    message.channel.send(`\`\`\`${returnedMsg}\`\`\``);
+  }
+});
+
