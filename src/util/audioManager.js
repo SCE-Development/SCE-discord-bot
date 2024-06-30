d0dc0f602523bce440f82021eb5ac5211c37cff0
vy234 <131425330+vy234@users.noreply.github.com>
2024-06-29 23:42:32 -0700

    fixed music play/pause

---
+++ src/util/audioManager.js
@@ -0,0 +1,23 @@
+let audioPlayer = null;
+let connection = null;
+let videoInfo = null
+let vidUrl = null;
+
+module.exports = {
+  getAudioPlayer: () => audioPlayer,
+  setAudioPlayer: (player) => {
+    audioPlayer = player;
+  },
+  getConnection: () => connection,
+  setConnection: (conn) => {
+    connection = conn;
+  },
+  getInfo: () => videoInfo,
+  setInfo: (info) => {
+    videoInfo = info;
+  },
+  getUrl: () => vidUrl,
+  setUrl: (url) => {
+    vidUrl = url;
+  }
+};
