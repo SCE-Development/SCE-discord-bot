ab443d84ed830480a2e962b160474507bdaa9bd1
evan ugarte <evanuxd@gmail.com>
2024-07-17 21:01:57 -0700

    quick lint

--- src/commands/member_services/pick.js
+++ src/commands/member_services/pick.js
@@ -1,51 +1,48 @@
 const Command = require('../Command');
 
 module.exports = new Command({
-  name: 'pick',
-  description: 'Start a raffle, given a list of names.\
+    name: 'pick',
+    description: 'Start a raffle, given a list of names.\
   They can ping the bot and pick a person.',
-  aliases: [],
-  example: 's!pick',
-  permissions: 'member',
-  category: 'member services',
-  disabled: false,
-  execute: async (message, args) => 
-  {
-    const names = [];
-    let currName = '';
-    /* 
-    initialize names with list of names
-    use math.rand to pick a name in the list
-    
-      */
-    function randomize(number) {
-      let result = Math.floor( Math.random() * (number - 1) );
-      return result;
-    }
+    aliases: [],
+    example: 's!pick',
+    permissions: 'member',
+    category: 'member services',
+    disabled: false,
+    execute: async (message, args) => {
+        const names = [];
+        let currName = '';
+        /* 
+        initialize names with list of names
+        use math.rand to pick a name in the list
+        
+          */
+        function randomize(number) {
+            let result = Math.floor(Math.random() * (number - 1));
+            return result;
+        }
 
-    if (args.length <= 1 || !args.includes('|')) 
-    {
-      message.channel.send(`❌ | ${message.member}, 
+        if (args.length <= 1 || !args.includes('|')) {
+            message.channel.send(`❌ | ${message.member}, 
         You need to have 2 or more names in the raffle, 
         AND separate the items with a '|'`);
-      return;
-    }
-    
-    args.forEach(arg => {
-      // if the string contains a '|', ignore it
-      if (arg === '|')
-      {
+            return;
+        }
+
+        args.forEach(arg => {
+            // if the string contains a '|', ignore it
+            if (arg === '|') {
+                names.push(currName);
+                currName = '';
+            } else {
+                currName += ` ${arg}`;
+            }
+        });
         names.push(currName);
-        currName = '';
-      } else {
-        currName += ` ${arg}`;
-      }
-    });
-    names.push(currName);
 
-    const rand = randomize(names.length);
-    const winner = names[rand];
-    message.channel.send(`🤔 |  ${message.member}, I pick${winner}!`);
+        const rand = randomize(names.length);
+        const winner = names[rand];
+        message.channel.send(`🤔 |  ${message.member}, I pick${winner}!`);
 
-  }
+    }
 });
